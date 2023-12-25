95b70d01dd42fe505afdcb8a496736595b904291
Srgr0 <66754887+Srgr0@users.noreply.github.com>
2023-12-25 22:39:58 +0900

    auto-detect archtecture

--- installer.sh
+++ installer.sh
@@ -6,6 +6,13 @@ read -p "Enter your Cloudflare Zone ID: " cfzone_id;
 read -p "Enter the hostname: (e.g. example.srgr0.com)" hostname;
 read -p "Enter the service: (e.g. http://127.0.0.1:8888)" service;
 
+# check architecture
+arch=$(uname -m)
+if [[ "$arch" != "x86_64" && "$arch" != "i386" && "$arch" != "i686" && "$arch" != "armv6l" && "$arch" != "armv7l" && "$arch" != "aarch64" ]]; then
+    echo "Unsupported architecture: $arch"
+    exit 1
+fi
+
 # verify apikey
 response=$(curl -s -X GET -w "%{http_code}" \
            -H "Authorization: Bearer $cf_apikey" \
@@ -51,13 +58,30 @@ get_token_response=$(curl -s -X GET \
 cftunnel_token=$(echo $get_token_response | jq -r '.result');
 
 # install cf tunnel
-read -p "Are you on an ARM system? (y/n): " is_arm;
-if [ "$is_arm" = "y" ]; then
-    wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64.deb;
-    sudo dpkg -i cloudflared-linux-arm64.deb;
-else
-    wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb;
-    sudo dpkg -i cloudflared-linux-amd64.deb;
-fi
+case $arch in
+    x86_64)
+        echo "Architecture: 64-bit (amd64)"
+        wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+        sudo dpkg -i cloudflared-linux-amd64.deb
+        ;;
+
+    i386 | i686)
+        echo "Architecture: 32-bit (x86)"
+        wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386.deb
+        sudo dpkg -i cloudflared-linux-386.deb
+        ;;
+
+    armv6l | armv7l)
+        echo "Architecture: ARM"
+        wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm.deb
+        sudo dpkg -i cloudflared-linux-arm.deb
+        ;;
+
+    aarch64)
+        echo "Architecture: ARM64 (aarch64)"
+        wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64.deb
+        sudo dpkg -i cloudflared-linux-arm64.deb
+        ;;
+esac
 
 sudo cloudflared service install $cftunnel_token;
